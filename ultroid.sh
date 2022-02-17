ab2c441aaeb27de7aad015f06a790b84116ca25b
djnakum6239 <80020359+djnakum6239@users.noreply.github.com>
2022-02-17 22:20:56 +0530

    Update ultroid.sh

--- ultroid.sh
+++ ultroid.sh
@@ -3,4 +3,4 @@ cp PR/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
-pip3 --no-cache-dir install telethon-tgcrypto
+
