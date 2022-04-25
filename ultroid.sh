4a1a206f87fa3b2e14a3d604c484b4a8ece4c053
djnakum6239 <80020359+djnakum6239@users.noreply.github.com>
2022-04-25 22:52:57 +0530

    Update ultroid.sh

--- ultroid.sh
+++ ultroid.sh
@@ -1,7 +1,10 @@
+
 git clone https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
+
 cp PR/.env /root/TeamUltroid/.env
+
 cd /root/TeamUltroid
-pip install -U -r requirements.txt
-pip3 install --no-cache-dir -r requirements.txt
-pip3 install av --no-binary av
-bash startup
+
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
+
+docker run --privileged --env-file .env --rm -i ultroid
